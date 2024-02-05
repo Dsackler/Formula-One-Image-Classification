18949e091f2e9fcbf558b65b970aac74fc6ca387
Dsackler <dsackler@lion.lmu.edu>
2024-02-04 17:22:10 -0800

    Fixed formatting issue, added styles, fixed CSS

--- UI/classify-image/src/App.js
+++ UI/classify-image/src/App.js
@@ -6,7 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Drag and Drop File Upload</h1>
+        <h1>Formula 1 Image Classifier</h1>
+        <p>Upload a clear image of Carlos Sainz Jr, Charles Leclerc, Daniel Ricciardo, Lewis Hamilton, or Sebastian Vettel and I will identify the driver!</p>
         <FileUpload />
       </header>
     </div>
--- UI/classify-image/src/components/FileUpload.js
+++ UI/classify-image/src/components/FileUpload.js
@@ -1,8 +1,10 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import axios from 'axios';
+import './FileUpload.css';
 
 const FileUpload = () => {
+  const [serverResponse, setServerResponse] = useState(null);
   const onDrop = useCallback(async (acceptedFiles) => {
     const file = acceptedFiles[0];
 
@@ -16,6 +18,8 @@ const FileUpload = () => {
           },
         });
         console.log('Server Response:', response.data);
+        const formattedName = response.data[0].split('_').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+        setServerResponse(formattedName)
       } catch (error) {
         console.error('Error uploading file:', error);
       }
@@ -24,10 +28,26 @@ const FileUpload = () => {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <div {...getRootProps()} className={`dropzone ${isDragActive ? 'active' : ''}`}>
-      <input {...getInputProps()} />
-      <p>Drag & drop a file here, or click to select a file</p>
+    <div>
+      <div className="ImageSection">
+        <img src="/images/carlos_pic.jpg" alt="Driver 1" />
+        <img src="/images/charles_pic.jpg" alt="Driver 2" />
+        <img src="/images/daniel_pic.jpg" alt="Driver 3" />
+        <img src="/images/lewis_pic.jpg" alt="Driver 4" />
+        <img src="/images/seb_pic.jpg" alt="Driver 4" />
+      </div>
+        <div {...getRootProps()} className={`dropzone ${isDragActive ? 'active' : ''}`}>
+          <input {...getInputProps()} />
+          <p>Drag & drop a file here, or click to select a file</p>
+      </div>
+      {serverResponse && (
+        <div>
+          <h2>You uploaded a picture of:</h2>
+          <h3>{serverResponse}</h3>
+        </div>
+      )}
     </div>
+    
   );
 };
 
